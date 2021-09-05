d5edec96fa4ca7c88d0f5c917e4c63c67bdc609e
Igor Engler <igorengler97@hotmail.com>
2021-09-04 21:43:35 -0300

    score system working

--- game.py
+++ game.py
@@ -1,21 +1,26 @@
 import pygame
 import sys
-
+import random
 
 def ball_animation():
-    global ball_speed_x
-    global ball_speed_y
+    global ball_speed_x, ball_speed_y, score
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
-    if ball.top <= 0 or ball.bottom >= screen_height:
+    if ball.top <= 0:
         ball_speed_y *= -1
+    if ball.bottom >= screen_height:
+        ball_restart()
+        score = 0
+        print(score)
     if ball.left <= 0 or ball.right >= screen_width:
         ball_speed_x *= -1
 
     if ball.colliderect(player):
         ball_speed_y *= -1
+        score += 1
+        print(score)
 
 def player_animation():
     player.x += player_speed
@@ -24,7 +29,12 @@ def player_animation():
         player.left = 0
     if player.right >= screen_width:
         player.right = screen_width
-    
+
+def ball_restart(): 
+    global ball_speed_y, ball_speed_x
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_x *= random.choice((1, -1))
+    #ball_speed_y *= random.choice((1, -1))
 
 # setup geral
 pygame.init()  # inicia todos os módulos pygame, necessário
@@ -32,23 +42,26 @@ clock = pygame.time.Clock()
 
 # criando a imagem principal
 screen_width = 1280
-screen_height = 960
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('pong')
 
 # retangulos do jogo
-# no meio da tela, com 30 pixels de altura e de largura
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width/2 - 60, screen_height - 60, 120, 10)
 
 # variaveis de velocidade
-ball_speed_x = 7
-ball_speed_y = 7
+ball_speed_x = 8 * random.choice((1, -1))
+ball_speed_y = 8
 player_speed = 0
 
 # cores
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
+white = (255,255,255)
+
+# variavel de pontuação
+score = 0
 
 while True:
     # lidando com input
@@ -58,26 +71,25 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_speed -= 10
+                player_speed -= 12
             if event.key == pygame.K_RIGHT:
-                player_speed += 10
+                player_speed += 12
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
-                player_speed += 10
+                player_speed += 12
             if event.key == pygame.K_RIGHT:
-                player_speed -= 10
+                player_speed -= 12
             
 
     ball_animation()
 
     player_animation()
     
-
     # aparencia dos objetos
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.ellipse(screen, light_grey, ball)
-
+    
     # atualizando a tela
     pygame.display.flip()
     clock.tick(60)  # limita o loop a rodar 60 vezes por segundo
